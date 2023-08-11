b6d7b7c2719e6f9a0dc716d32523046779424e99
Teotonio José de Souza Junior <102707827+teotoniojr@users.noreply.github.com>
2023-08-11 08:28:34 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+let cor;
+let posicaoHorizontal;
+let posicaoVertical;
+let tamanho;
+
+
+function setup() {
+  createCanvas(400, 400);
+  background("orange");
+  cor = color(random(0, 255), random(0, 255), random(0, 255));
+  posicaoHorizontal = 200;
+  posicaoVertical = 200;
+   tamanho = 50;
+}
+
+function draw() {
+  
+  stroke(cor);
+  fill(cor);
+  circle(posicaoHorizontal, posicaoVertical, tamanho);
+  
+  if(mouseX < posicaoHorizontal){
+    posicaoHorizontal -= 1
+  }
+  
+  if(mouseX > posicaoHorizontal){
+    posicaoHorizontal += 1
+  }
+  
+  if(mouseY < posicaoVertical){
+    posicaoVertical -= 1
+  }
+  
+  if(mouseY > posicaoVertical){
+    posicaoVertical += 1
+  }
+  if (mouseIsPressed){
+    cor = color(random(0, 255), random(0, 255), random(0, 255), random(0, 100));
+  }
+}
+
+
+
+
+
